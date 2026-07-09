23a342744c0a5439fa3230bcff7a825663a63442
koten4ik <koten4ik@gmail.com>
2026-07-09 13:50:21 +0300

    speed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             Modules\ZSupport\App\Middleware\RegisterRequestMiddleware::class,
         ]);
+        $middleware->redirectGuestsTo(fn (Request $request) => route('user.auth.loginPage'));
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->report(function (Throwable $exception) {
